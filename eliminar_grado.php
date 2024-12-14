bd5468ca795cf971aa450ed865d5f2f3fc39337d
TLujan101 <t0m4luj4c@gmail.com>
2024-12-14 09:18:32 -0500

    Update eliminar_grado.php

--- eliminar_grado.php
+++ eliminar_grado.php
@@ -28,7 +28,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar Grado</title>
     <link rel="shortcut icon" href="css/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
     <div>
